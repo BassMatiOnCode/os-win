7655b76bf241f4e75d31aabba1938f05b8f15706
bassmationcode <BassmatiOnCode@freenet.de>
2022-12-02 13:21:12 +0100

    start scripts updated according to configuration changes

--- start-dev.bat
+++ start-dev.bat
@@ -1,3 +1,4 @@
-start "starting vs" "c:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\ide\devenv.exe" "%cd%"
-start /min "Deno Webserver" deno-file-server
-start start-msedge.bat
+call start-vs.bat
+call start-webserver.bat
+call start-msedge.bat
+
